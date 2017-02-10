ace97e524f2eba9058b21ec3b732c3de075a2d0a
ReGadex <regadex@icloud.com>
2017-02-10 17:06:12 +0900

    Create CommonMethod.java

---
+++ CommonMethod.java
@@ -0,0 +1,18 @@
+package fx;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class CommonMethod {
+	//現在時刻を取得する関数
+	public static String getDate()
+	{
+		String dateStr = "";
+		Calendar calendar = Calendar.getInstance(Locale.JAPAN);
+        DateFormat dateformat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+        dateStr = dateformat.format(calendar.getTime());
+        return dateStr;
+	}
+}
